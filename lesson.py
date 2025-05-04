5fc50b657589615a41c2ddd5700b5db825057207
Shomopriyo Karmaker <shomopriyo.k@gmail.com>
2025-05-04 23:00:46 +0600

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,38 @@
+print("Hello Guyss")
+print("Name: Shomopriyo")
+print("Country: Bangladesh")
+print("I read in class 6")
+print("I am from Dhaka")
+print("Fridend: Yann")
+print("Hobby : Cricketer")
+print("I have a elder Sister")
+print("She reads in class 12.")
+print("My date of birth is 07.02.2013.")
+print("Read in Ideal School and College In Dhaka")
+print("Home Town Nilphamari.")
+print("The member of many School's culb.")
+
+
+
+
+# Variable
+
+Name ="      Shomopriyo"
+print(Name)
+Grade = "     6 "
+print(Grade)
+School= "    Ideal School And College"
+print(School)
+
+
+
+# Variable + Text
+
+name = "Shomopriyo."
+Country = "Bangladesh."
+
+print("My name is",name,"I am from",Country)
+
+print ( f"My name is{name} and I am from{Country}")
+
+
