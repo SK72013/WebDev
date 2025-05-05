f0343ba08227261e43bebaa014b1eeaf4f9a0593
Shomopriyo Karmaker <shomopriyo.k@gmail.com>
2025-05-05 21:51:43 +0600

    Add files via upload

---
+++ DataType .py
@@ -0,0 +1,61 @@
+# Data Type
+
+Name = "Aron" #String
+Grade = 6 #Integer
+Marks = 98.3 # Float
+Pass = True # Boolean
+
+
+print (type(Name))
+print(type(Grade))
+print(type(Marks))
+print(type(Pass))
+
+
+a = 5
+b = 3
+
+#Arithmatic Ops
+
+print(a+b)
+print( a*b)
+print(a-b)
+print(a/b)
+print(a**b)
+
+
+#Logical Ops
+
+print(a>b)
+print(a<b)
+# print(a=b)
+# print(a>=b)
+# print(a<=b)
+# print(a==b)
+# print(a!=b)
+
+
+
+text = "Codingal"   #String
+
+#Index
+print(text[0])
+print(text[1])
+print(text [2])
+print(text [3])
+print(text[4])
+print(text[5])
+print(text[6])
+print(text[7])
+
+
+
+#Basic Functions
+
+print(text.upper())
+print(text.lower())
+print(text.swapcase())
+
+
+
+
