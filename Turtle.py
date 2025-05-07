5aad47cb1029ad641f2c7cdf35fc15f10ff1fed2
Shomopriyo Karmaker <shomopriyo.k@gmail.com>
2025-05-07 22:55:25 +0600

    Add files via upload

---
+++ Turtle.py
@@ -0,0 +1,37 @@
+#Turtle - Graphical Design Pattern
+
+
+import turtle
+
+
+screen = turtle.Screen()
+screen.bgcolor("cyan")
+screen.title("Let's draw with Shomopriyo")
+
+pen = turtle.Turtle()
+
+pen.forward(200)
+pen.left(90)
+pen.forward(200)
+pen.left(90)
+pen.forward(200)
+pen.left(90)
+pen.forward(200)
+pen.right(70)
+pen.forward(80)
+
+pen.forward(200)
+pen.right(90)
+pen.forward(200)
+pen.right(90)
+pen.forward(278)
+pen.right(90)
+pen.forward(200)
+
+
+
+
+
+
+
+turtle.done()
