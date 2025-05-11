71ab24d4276ff60978c9fae013f9bac2057b6dfb
Shomopriyo Karmaker <shomopriyo.k@gmail.com>
2025-05-11 22:52:36 +0600

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,74 @@
+# age = 12
+
+# if age>18 :
+#     print("You are eligible for vote!")
+# else:
+#     print("You are not eligible for vote")
+
+
+# day = 10
+
+
+# if day == 1:
+#     print("Sunday")
+# elif day == 2:
+#     print("Monday")
+# elif day == 3:
+#     print("Tuesday")
+# elif day == 4:
+#     print("Wednesday")
+# elif day == 5:
+#     print("Thursday")
+# elif day == 6:
+#     print("Friday")
+# elif day == 7:
+#     print("Saturday")
+# else :
+#     print("Invalid")
+       
+
+
+# name = input("Enter Your Name:")
+# print("Welcome",name)
+
+# age = int(input("Enter Age :" ))
+# print(age)
+
+# if age > 18:
+#     print("You are eligiable")
+# else:
+#     print("Sorry,Not eligiable")
+
+
+#List
+
+#Collection of item
+
+fruits = ["Apple","Banana","Mango","Cherry","Guava","Grape","Peach","Watermelon","Jackfruit"]
+
+
+
+print(fruits)
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+print(fruits[4])
+print(fruits[5])
+print(fruits[6])
+print(fruits[7])
+print(fruits[8])
+print(fruits[9])
+
+
+ 
+
+
+
+
+
+
+
+     
+
