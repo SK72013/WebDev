69a4bfb030392214a568fc7dc7c1cc68f0c733f0
Shomopriyo Karmaker <shomopriyo.k@gmail.com>
2025-09-01 21:53:24 +0600

    Add files via upload

---
+++ leeson2.py
@@ -0,0 +1,48 @@
+
+print("Hello World")
+
+#Operator
+
+print("Arithmatic")
+
+a = 5
+b =10
+
+print(a+b)
+print(a-b)
+print(a//b)
+print(a*b)
+print(a/b)
+
+print("Comparison")
+
+print(a==b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+
+
+age = 13
+
+if(age>18) :
+    print("Eligiable for Voter Card")
+else:
+    print("Not Eligiable")
+
+number = 69
+
+if number>0 :
+    print("Positive Number")
+else:
+    print("Negative Number")
+
+
+
+number = 9612354
+
+if (number%2 == 0) :
+    print("Even")
+else:
+    print("Odd")
