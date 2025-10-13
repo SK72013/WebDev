51ef88e389b3ead28f098ffc392a23325dc1358f
Shomopriyo Karmaker <shomopriyo.k@gmail.com>
2025-10-13 21:46:21 +0600

    Add files via upload

---
+++ Myturtle.py
@@ -0,0 +1,23 @@
+
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("orange")
+screen.setup(300,400)
+
+
+pen = turtle.Turtle()
+
+
+
+
+pen.forward(120)
+pen.left(125)
+pen.forward(110)
+pen.left(115)
+pen.forward(109)
+
+
+
+
+turtle.done()
