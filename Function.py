4f970f34e6da21ea77fe7f78d4578ee046d3e57e
Shomopriyo Karmaker <shomopriyo.k@gmail.com>
2025-10-26 22:43:52 +0600

    Add files via upload

--- Function.py
+++ Function.py
@@ -1,83 +1,33 @@
-#Function
-def GM():
-    print("Good Morning!")
 
-def GN():
-    print("Good Evening")
-def GE():
-    print("Good Night")
+# def wish():
+#     print("Hello cricketer !!")
 
+# wish()
+# wish() 
 
-# GM()
-# GN()
-# GE()
-
-#Arguments / Parameter
-
-def  GM(name):
-    print("Good Morning",name)
-
-# GM("Shomopriyo")
-# GM("Tashenul")
-# GM("Rohan")
+# def BSA():
+#     print("Good Morning Cricketers.")
 
 
-# def Chat(f1,f2,f3):
-#     print(f1," : Hey Everyone! How are you ?")
-#     print(f2," : Yeah,I am doging great. What about you ? ")
-#     print(f3," : I also doing good. ")
-#     print(f1," : Okey, Bye")
-#     print(f2," : Bye Bye!")
-#     print(f3," : Take care!....")
+# BSA()
+# BSA()
+# BSA()
 
-# Chat("Shomopriyo","Rohan","Yann")
+# def say(name):
+#     print("Happy New year",name)
 
-
-# def Add(a,b):
-#     print(a+b)
-# def Sub (a,b):
-#     print(a-b)
-# def Mun (a,b):
-#     print(a*b)
-# def Div (a,b):
-#     print(a/b)
-
-# p = 7
-# q = 5
-
-# Add (p,q)
-# Sub (p,q)
-# Mun (p,q)
-# Div (p,q)
-
-# def A():
-#     print("Inside A function")
-#     B()
-
-# def B():
-#    print("Inside B function")
-#    C()
-
-# def C():
-#      print("Inside C function")
-
-# A()
-
-def wish():
-    return "Happy Birthday"
-
-a = wish()
-
-def Square(a):
-    return a**2
-
-a = Square(2)
-b = Square(3)
-c = Square(4)
-
-print(a,b,c)
-    
+# say("Shomopriyo")
 
 
+def Add(a,b):
+    print (a+b)
+Add(12,23)
 
-    
+def Division(a,b):
+    print(a/b)
+Division(40,2)
+
+def Multiply(a,b):
+    print(a*b)
+Multiply(120,52)
+
