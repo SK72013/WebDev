00353458151aa921e0a4ae115448d3076c8f0417
Shomopriyo Karmaker <shomopriyo.k@gmail.com>
2025-10-27 21:44:58 +0600

    Add files via upload

---
+++ Function2.py
@@ -0,0 +1,48 @@
+
+# def Division(a,b):
+#     return a/b
+
+# result = Division(1200,10) 
+# print(result)
+
+
+# def wish():
+#     retrun "Rest in Peace"
+
+# print(wish())
+# bday = wish()
+# print(bday)
+
+for number in range(5,10):
+    print(Counter,"Hello")
+
+for number in range(5,10):
+    if counter==4:
+        break
+    print(counter)
+
+Fruits =["Apple","Banana","Cherry","Watermelon","Papaya"]
+
+for item in fruits:
+    if item=="Cherry":
+        print("Cherry Found!")
+        break
+    print(item)
+
+
+
+for i in range(1,10):
+    if i == 3:
+        continue
+    print(i)
+
+
+
+a = input("Enter a word:  ")
+
+for i in a:
+    if (i=='a'):
+      print("A is found")
+      break
+    else:
+        print("A is not found")
